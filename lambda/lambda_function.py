01f2aa534b6c1111bc46c0f02f1d967148a56942
DenisWm <denismamoni@hotmail.com>
2024-12-15 09:22:15 -0300

    a

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -33,7 +33,7 @@ def lambda_handler(event, context):
         aws_access_key_id='test', 
         aws_secret_access_key='test', 
         region_name='us-east-1', 
-        endpoint_url='http://localhost:4566'
+        endpoint_url='http://localstack:4566'
     )
 
     try:
