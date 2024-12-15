89ccad1a4c69da3b97d69b570eb0da44e4186fc3
DenisWm <denismamoni@hotmail.com>
2024-12-15 08:44:22 -0300

    a

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,17 +19,75 @@ resource "aws_cognito_user_pool" "user_pool" {
   schema {
     attribute_data_type = "String"
     name                = "email"
-    required            = false
-    mutable             = false
+    required            = true
+    mutable             = true
   }
 
-  schema {
-    attribute_data_type = "String"
-    name                = "cpf"
-    required            = false
-    mutable             = false
-  }
-
-  username_attributes = ["email"]
+  username_attributes = []
+  auto_verified_attributes = ["email"]
 }
 
+resource "aws_lambda_function" "check_user_by_cpf" {
+  filename         = "lambda.zip" 
+  function_name    = "check-user-by-cpf"
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
+  timeout          = 10
+
+  source_code_hash = filebase64sha256("lambda.zip")
+  
+  environment {
+    variables = {
+      USER_POOL_ID = aws_cognito_user_pool.user_pool.id
+    }
+  }
+}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "API-Gateway-CheckUser"
+  description = "API Gateway para chamar a Lambda check-user-by-cpf"
+}
+
+resource "aws_api_gateway_resource" "user_check" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "user-check"
+}
+
+resource "aws_api_gateway_method" "get_user_check" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.user_check.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.user_check.id
+  http_method             = aws_api_gateway_method.get_user_check.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.check_user_by_cpf.arn}/invocations"
+}
+
+resource "aws_lambda_permission" "lambda_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.check_user_by_cpf.function_name
+  principal     = "apigateway.amazonaws.com"
+}
+resource "aws_api_gateway_deployment" "api_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+
+
+resource "aws_api_gateway_stage" "api_stage" {
+  deployment_id = aws_api_gateway_deployment.api_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "prod"
+}
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,15 +1,5 @@
 variable "region" {
-  default = "us-east-1"
-}
-
-variable "user_pool_name" {
-  default = "UserPoolCPF"
-}
-
-variable "user_pool_client_name" {
-  default = "UserPoolClient"
-}
-
-variable "user_pool_client_names" {
-  default = "UserPoolClients"
+  description = "A região da AWS onde os recursos serão criados"
+  type        = string
+  default     = "us-east-1"
 }
